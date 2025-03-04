c244678b20a8550343813416d016583cd25769c1
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-03-04 16:55:42 +0100

    ADDED THE GUARD AND DATABASE CREATION

---
+++ Frontend/RegistreCurricular/src/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const token = localStorage.getItem('authToken');
+
+  if (!token) {
+    router.navigate(['/login']);
+    return false;
+  }
+
+  try {
+    const decodedToken: any = jwtDecode(token);
+
+    if (decodedToken.role === 'user') {
+      return true;
+    } else {
+      router.navigate(['/login']);
+      return false;
+    }
+  } catch (error) {
+    router.navigate(['/login']);
+    return false;
+  }
+};
