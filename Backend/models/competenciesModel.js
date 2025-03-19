27500d1c15d573733909d1b3d02451c3239ff251
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-03-19 17:48:30 +0100

    Created part to generate Subjects on the Template

--- Backend/models/competenciesModel.js
+++ Backend/models/competenciesModel.js
@@ -1,34 +1,46 @@
-//models/competenciesModel.js
+// models/competenciesModel.js
 const sql = require('mssql');
 const config = require('../config/db');
 
-exports.insertData = async (jsonData) => {
+exports.insertData = async (jsonData, templateName) => {
   try {
     let pool = await sql.connect(config);
     
-    for (const materia of jsonData) {
-      const resultMateria = await pool.request()
-        .input('Name', sql.VarChar(50), materia.materia)
-        .input('Type', sql.TinyInt, materia.tipus === 'transversal' ? 1 : 0)
-        .query('INSERT INTO SubjectsTPL (Name, Type) OUTPUT INSERTED.UUID VALUES (@Name, @Type)');
-
-      const materiaUUID = resultMateria.recordset[0].UUID;
-
-      for (const comp of materia.competències) {
-        const resultComp = await pool.request()
-          .input('SubjectName', sql.VarChar(20), comp.titol)
-          .input('UUIDSubject', sql.UniqueIdentifier, materiaUUID)
-          .query('INSERT INTO CompetenciesTPL (SubjectName, UUIDSubject) OUTPUT INSERTED.UUID VALUES (@SubjectName, @UUIDSubject)');
-
-        const compUUID = resultComp.recordset[0].UUID;
-
-        for (const criteri of comp.criteris) {
-          await pool.request()
-            .input('UUIDCompetencie', sql.UniqueIdentifier, compUUID)
-            .input('Description', sql.VarChar(sql.MAX), criteri)
-            .query('INSERT INTO CriteriaTPL (UUIDCompetencie, Description) VALUES (@UUIDCompetencie, @Description)');
-        }
+    for (const subject of jsonData) {
+      const subjectName = subject.materia;
+      const numericType = parseInt(subject.type, 10);
+      
+      const selectQuery = `
+        SELECT UUID 
+        FROM SubjectsTPL 
+        WHERE Name = @Name AND TemplateName = @TemplateName
+      `;
+      const selectResult = await pool.request()
+        .input('Name', sql.VarChar(50), subjectName)
+        .input('TemplateName', sql.VarChar(20), templateName)
+        .query(selectQuery);
+      
+      let subjectUUID;
+      if (selectResult.recordset.length > 0) {
+        subjectUUID = selectResult.recordset[0].UUID;
+      } else {
+        const insertQuery = `
+          INSERT INTO SubjectsTPL (Name, TemplateName, Type)
+          OUTPUT INSERTED.UUID
+          VALUES (@Name, @TemplateName, @Type)
+        `;
+        const insertResult = await pool.request()
+          .input('Name', sql.VarChar(50), subjectName)
+          .input('TemplateName', sql.VarChar(20), templateName)
+          .input('Type', sql.TinyInt, numericType)
+          .query(insertQuery);
+        subjectUUID = insertResult.recordset[0].UUID;
       }
+      
+      // Aquí podríem continuar treballant amb Competencies si ho necessités.
+      // Per ara només ens centrem en SubjectsTPL segons les teves instruccions.
+      
+      console.log(`Materia ${subjectName} té UUID: ${subjectUUID}`);
     }
   } catch (error) {
     throw error;
