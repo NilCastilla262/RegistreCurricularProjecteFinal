39d90306da91699b661bd3488cc73e384c128bb8
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-03-24 19:17:30 +0100

    CREATED all necessary files to create SDAs

--- Frontend/RegistreCurricular/src/tests/subject.service.spec.ts
+++ Frontend/RegistreCurricular/src/tests/subject.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import { SubjectService } from './subject.service';
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe('SubjectService', () => {
+  let service: SubjectService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    service = TestBed.inject(SubjectService);
   });
 
   it('should be created', () => {
