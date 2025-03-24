891c25991db1a2127bdd04cd046cafac854b61dd
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-03-24 19:30:35 +0100

    CREATED sda service

--- Frontend/RegistreCurricular/src/services/sda.service.ts
+++ Frontend/RegistreCurricular/src/services/sda.service.ts
@@ -1,9 +1,33 @@
+// sda.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { SdaModel } from '../models/sda/sda.model';
+import { SdaDTO } from '../models/sda/sda.dto';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SdaService {
+  private baseUrl = environment.baseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getSdaByUUID(uuid: string): Observable<SdaModel> {
+    return this.http.get<any>(`${this.baseUrl}/sda/${uuid}`).pipe(
+      map(apiResponse => SdaDTO.fromApi(apiResponse))
+    );
+  }
+
+  createSda(sda: SdaModel): Observable<any> {
+    const body = SdaDTO.toApi(sda);
+    return this.http.post(`${this.baseUrl}/sda`, body);
+  }
+
+  getAllSdas(): Observable<SdaModel[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/sda`).pipe(
+      map(apiArray => SdaDTO.fromApiArray(apiArray))
+    );
+  }
 }
