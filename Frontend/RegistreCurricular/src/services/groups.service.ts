beeec3ddf9ffee155acc7fb6afbd405b17ce6263
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-03-26 20:46:03 +0100

    IMPLEMENTED group.service

---
+++ Frontend/RegistreCurricular/src/services/groups.service.ts
@@ -0,0 +1,24 @@
+// groups.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Observable, map } from 'rxjs';
+import { GroupModel } from '../models/groups/group.model';
+import { GroupDTO } from '../models/groups/group.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupsService {
+  private baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getGroupsFromUser(userUUID: string): Observable<GroupModel[]> {
+    return this.http
+      .get<any[]>(`${this.baseUrl}/groups?userUUID=${userUUID}`)
+      .pipe(
+        map(apiArray => GroupDTO.fromApiArray(apiArray))
+      );
+  }
+}
