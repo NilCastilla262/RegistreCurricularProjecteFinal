81ae32549e24721db8f3e54483d37f9a936e55a5
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-04-22 16:21:59 +0200

    UPDATED create-resume to redirect to the show-resume with multiple UUID

--- Frontend/RegistreCurricular/src/components/create-resume/create-resume.component.ts
+++ Frontend/RegistreCurricular/src/components/create-resume/create-resume.component.ts
@@ -2,28 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GroupModel } from '../../models/groups/group.model';
 import { GroupsService } from '../../services/groups.service';
-import { SdaService } from '../../services/sda.service';
 import { CommonModule } from '@angular/common';
-import { CoursesService } from '../../services/courses.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-resume',
   standalone: true,
-  imports: [CommonModule, FormsModule ],
+  imports: [CommonModule, FormsModule],
   templateUrl: './create-resume.component.html',
   styleUrl: './create-resume.component.css'
 })
 export class CreateResumeComponent implements OnInit {
   groups: GroupModel[] = [];
   selectedGroupUUID: string = '';
-  errorMessage: string = '';
-  resum: any;
+  errorMessage = '';
 
   constructor(
     private groupsService: GroupsService,
-    private sdaService: SdaService,
-    private CoursesService: CoursesService,
     private router: Router
   ) {}
 
@@ -31,31 +26,23 @@ export class CreateResumeComponent implements OnInit {
     this.loadGroups();
   }
 
-  loadGroups(): void {
+  private loadGroups(): void {
     this.groupsService.getGroupsFromUser().subscribe({
-      next: (groups) => { 
-        this.groups = groups; 
-      },
-      error: (err) => { 
-        this.errorMessage = 'Error carregant grups: ' + (err.error?.message || err.message); 
-      }
+      next: groups => this.groups = groups,
+      error: err => this.errorMessage = 'Error carregant grups: ' + (err.error?.message || err.message)
     });
   }
 
-  getResum(): void {
+  goToResume(): void {
     if (!this.selectedGroupUUID) {
       this.errorMessage = 'Selecciona un grup.';
       return;
     }
-    
-    this.sdaService.getResum([this.selectedGroupUUID]).subscribe({
-        next: (data) => {
-        this.resum = data;
-        console.log('Resum:', this.resum);
-      },
-      error: (err) => {
-        this.errorMessage = 'Error obtenint el resum: ' + (err.error?.message || err.message);
-      }
-    });
+
+    const uuids = [ this.selectedGroupUUID ];
+    this.router.navigate(
+      ['show-resume'],
+      { queryParams: { groups: uuids.join(',') } }
+    );
   }
 }
