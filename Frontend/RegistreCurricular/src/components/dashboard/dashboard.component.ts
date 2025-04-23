ba2864cfe32455ea8db396ea8890f3f5f13dea45
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-04-23 16:55:18 +0200

    UPDATED dashboard to difference between teacher and adminCenter (Role 1 or 2)

--- Frontend/RegistreCurricular/src/components/dashboard/dashboard.component.ts
+++ Frontend/RegistreCurricular/src/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,6 +9,18 @@ import { RouterModule } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  centerRole = 0;
 
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if (!token) return;
+
+    try {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      this.centerRole = payload.centerRole || 0;
+    } catch (e) {
+      console.warn('No s’ha pogut processar el token JWT.', e);
+    }
+  }
 }
