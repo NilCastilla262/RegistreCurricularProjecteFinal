7d0e6955482706b85bb5ec16bcaf4b3f42246fde
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-05-15 18:49:31 +0200

    CREATED new component navbar to show always

---
+++ Frontend/RegistreCurricular/src/components/navbar/navbar.component.ts
@@ -0,0 +1,39 @@
+import { Component, HostListener } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule }   from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+
+export class NavbarComponent {
+  showProfileMenu = false;
+
+  constructor(private router: Router) {}
+
+  goDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  toggleProfileMenu() {
+    this.showProfileMenu = !this.showProfileMenu;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  @HostListener('document:click', ['$event.target'])
+  onClickOutside(target: HTMLElement) {
+    const inside = target.closest('.profile-container');
+    if (!inside) {
+      this.showProfileMenu = false;
+    }
+  }
+}
+
