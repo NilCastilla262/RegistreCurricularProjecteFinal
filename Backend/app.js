43d62fd11a4a784b029cd9a968ae55473b4adf99
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-05-15 15:58:51 +0200

    CREATED new files to save cors, rate limiter and the health check from the database and REFACTORED app.js to use them

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,15 @@
-// app.js
+// src/app.js
 import express from "express";
 import cors from "cors";
-import rateLimit from "express-rate-limit";
-import { getConnection } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import apiRoutes from "./routes/index.js";
 import { verifyToken } from "./middlewares/authMiddleware.js";
 import requestLogger from "./middlewares/requestLogger.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import { generateSwaggerDocs, mountSwagger } from "./config/swagger.js";
+import { limiter } from "./config/rateLimiter.js";
+import { corsOptions } from "./config/corsConfig.js";
+import dbHealthCheck from "./middlewares/dbHealthCheckMiddleware.js";
 
 const app = express();
 
@@ -16,22 +17,9 @@ generateSwaggerDocs();
 
 mountSwagger(app);
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 500,
-  message: "Has superat el límit de peticions per minut",
-});
-
 app.use(limiter);
 
-app.use(
-  cors({
-    origin: "http://localhost:4200",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["Authorization"],
-  })
-);
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -41,19 +29,7 @@ app.use("/apirc/v1/auth", authRoutes);
 app.use(verifyToken);
 app.use(requestLogger);
 
-app.use(async (req, res, next) => {
-  try {
-    const pool = await getConnection();
-    if (!pool.connected) {
-      const err = new Error("Database not connected");
-      err.status = 503;
-      throw err;
-    }
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
+app.use(dbHealthCheck);
 
 app.use("/apirc/v1", apiRoutes);
 
