dcf074b12ead31956de0e8ead8e3069ac43b5371
NilCastilla262 <ncastilla262@boscdelacoma.cat>
2025-05-16 16:32:58 +0200

    DELETED las endpoint created and CREATED new endpoint to return all avaliable centers for user

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,12 +1,12 @@
 // routes/authRoutes.js
 import express from "express";
-import { googleLoginController, chooseCenterController } from "../controllers/authController.js";
+import { googleLoginController, chooseCenterController, listMyCentersController } from "../controllers/authController.js";
 import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/google-login", googleLoginController);
 router.post("/choose-center", chooseCenterController);
-router.get("/centers", verifyToken, getCentersForUserController);
+router.get("/my-centers", verifyToken, listMyCentersController);
 
 export default router;
